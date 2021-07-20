63e5468a39ce85f29ddcd13017728d32cdd4cc77
mylit <vlmatveev5@gmail.com>
2021-07-20 19:53:36 +0300

    exe 2

---
+++ Exercise_2/exe_2.py
@@ -0,0 +1,7 @@
+prince = 99
+print(prince)
+inte = 5
+floa = 2.0
+print(type(inte))
+print(type(floa))
+print(type(inte + floa))
---
+++ Exercise_3/exe_3.py
@@ -0,0 +1,11 @@
+second_in_minute = 60
+minute_in_hour = 60
+second_per_hour = second_in_minute * minute_in_hour
+print('second per day :', second_per_hour * 24)
+
+second_per_day = second_per_hour * 24
+print('second_per_day : ', second_per_day)
+
+print('floating point division:', second_per_day/second_per_hour)
+print('integer division:', second_per_day//second_per_hour)
+
